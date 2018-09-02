d3e4375eb22b28d664f2db5b1d43b83f44a4b46f
Julián Picón <ja.picon@uniandes.edu.co>
2018-09-02 17:00:23 -0500

    Fronto book - Paso 1

---
+++ Book-Front/src/app/book/book.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Book } from './book';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = "../../assets/";
+const books = '/books.json';
+
+/**
+* The service provider for everything related to books
+*/
+@Injectable()
+export class BookService {
+    
+    /**
+    * Constructor of the service
+    * @param http The HttpClient - This is necessary in order to perform requests
+    */
+    constructor(private http: HttpClient) { }
+    
+    /**
+    * Returns the Observable object containing the list of books retrieved from the API
+    * @returns The list of books in real time
+    */    
+    getBooks(): Observable<Book[]> {
+        return this.http.get<Book[]>(API_URL + books);
+    }
+}
---
+++ Book-Front/src/app/editorial/editorial.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Editorial } from './editorial';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = "../../assets/";
+const editorials = '/editorials.json';
+
+/**
+* The service provider for everything related to editorials
+*/
+@Injectable()
+export class EditorialService {
+    
+    /**
+    * Constructor of the service
+    * @param http The HttpClient - This is necessary in order to perform requests
+    */
+    constructor(private http: HttpClient) { }
+    
+    /**
+    * Returns the Observable object containing the list of editorials retrieved from the API
+    * @returns The list of books in real time
+    */
+    getEditorials(): Observable<Editorial[]> {
+        return this.http.get<Editorial[]>(API_URL + editorials);
+    }
+    
+}
